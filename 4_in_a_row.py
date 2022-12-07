022e1d0fd7f7cf0a9772f9f678733bfe497bff17
Felix Hututuc <felix.hututuc@gmail.com>
2022-12-07 16:40:18 +0200

    Fix piece placing bug + add game win check (wip)

--- 4_in_a_row.py
+++ 4_in_a_row.py
@@ -11,16 +11,16 @@ except ImportError:
     import numpy as np
 
 
-try:
-    import pygame
-except ImportError:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pygame'])
+# try:
+#     import pygame
+# except ImportError:
+#     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pygame'])
 
-    import pygame
+#     import pygame
 
 
-LINE_COUNT = 2
-COLL_COUNT = 2
+LINE_COUNT = 4
+COLL_COUNT = 4
 
 
 def log(msg, errorMsg = False, endLine=True):
@@ -35,6 +35,16 @@ def log(msg, errorMsg = False, endLine=True):
 
 def init_board(rows, colls):
     return np.zeros((rows, colls))
+
+
+def find_first_empty(board, coll):
+    first_empty = LINE_COUNT - 1
+    for row in range(LINE_COUNT - 1):
+        if board[row + 1][coll] != 0:
+            first_empty = row
+            break
+
+    return first_empty
 
 
 def place_piece(board, row, coll, player):
@@ -62,10 +72,10 @@ def place_piece_onefunc(board, coll, player):
         log("Chosen collumn out of range")
         return False
 
-    first_empty = COLL_COUNT - 1
-    for row in range(LINE_COUNT - 1, -1, -1):
-        if board[row][coll] != 0:
-            first_empty = row - 1
+    first_empty = LINE_COUNT - 1
+    for row in range(LINE_COUNT - 1):
+        if board[row + 1][coll] != 0:
+            first_empty = row
             break
 
     if first_empty < 0 or first_empty >= LINE_COUNT:
@@ -76,19 +86,50 @@ def place_piece_onefunc(board, coll, player):
     return True
 
 
-def is_draw(board):
+def is_draw(board) -> bool:
     if any([True if 0 in board[i] else False for i in range(LINE_COUNT)]):
         return False
     return True
+
+
+def is_win_horizontally(board, player):
+    for row in board:
+        for coll_iter in range(COLL_COUNT - 3):
+            if all([piece == player
+                    for piece in [row[coll_iter + seq_iter] for seq_iter in range(4)]]):
+                return True
+
+    return False
+
+
+def is_win(board, player) -> bool:
+
+    # horizontally and vertically
+    if is_win_horizontally(board, player) or is_win_horizontally(np.transpose(board), player):
+        return True
+    return False
+
+    # diagonalls
 
 
 if __name__ == '__main__':
     board = init_board(LINE_COUNT, COLL_COUNT)
     # board = np.flip(board)
     print(board)
-    place_piece(board, 0, 1)
-    place_piece(board, 0, 2)
-    place_piece(board, 1, 1)
-    place_piece(board, 1, 1)
+    place_piece_onefunc(board, 0, 1)
+    place_piece_onefunc(board, 0, 1)
+    place_piece_onefunc(board, 1, 1)
+    place_piece_onefunc(board, 1, 1)
+    place_piece_onefunc(board, 0, 1)
+    place_piece_onefunc(board, 0, 1)
+    place_piece_onefunc(board, 3, 1)
+    place_piece_onefunc(board, 2, 2)
+
+    # board[1][1] = 1
+    # board[0][1] = 1
+
+
     print(board)
     print(is_draw(board))
+    print(board)
+    print(is_win(board, 1))
